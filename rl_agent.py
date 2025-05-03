32af58c440c0c1944028fd8294244d2bdf732a63
yatla2 <yatla2@uic.edu>
2025-05-02 23:33:22 -0500

    minor fix

--- rl_agent.py
+++ rl_agent.py
@@ -50,14 +50,14 @@ class ReplayMemory(object):
         return len(self.memory)
 
 class WSN_agent:
-    def __init__(self, sensor_ids, sampling_freq = 3, observation_time=10, transmission_size=4*1024, file_lines_per_chunk=5, transmission_frame_duration=1, BATCH_SIZE = 64, GAMMA = 0.7, EPS_START = 1, EPS_END = 0.0, EPS_DECAY = 400, TAU = 0.01, LR = 0.25e-3, recharge_thresh=0.2, max_steps=100, num_episodes=10, train_every=512, local_mininet_simulation=True, server_ip="", server_port=""):
+    def __init__(self, sensor_ids, sampling_freq = 3, observation_time=10, transmission_size=4*1024, file_lines_per_chunk=5, transmission_frame_duration=1, BATCH_SIZE = 64, memory_capacity=256, GAMMA = 0.7, EPS_START = 1, EPS_END = 0.0, EPS_DECAY = 400, TAU = 0.01, LR = 0.25e-3, recharge_thresh=0.2, max_steps=100, num_episodes=10, train_every=512, local_mininet_simulation=True, server_ip="", server_port=""):
         self._env = WSNEnvironment(max_steps=max_steps, sampling_freq=sampling_freq, sensor_ids=sensor_ids, observation_time=observation_time, transmission_size=transmission_size, transmission_frame_duration=transmission_frame_duration, file_lines_per_chunk=file_lines_per_chunk, recharge_thresh=recharge_thresh, device=device, num_episodes=num_episodes, local_mininet_simulation=local_mininet_simulation, server_ip=server_ip, server_port=server_port) 
         self._num_sensors = len(sensor_ids) 
         self._sampling_freq = sampling_freq # Number of possible transmission frequencies
         self._n_observations = self._num_sensors*self._num_sensors + 2*self._num_sensors
         self._recharge_thresh = recharge_thresh
         self._num_episodes = num_episodes
-        # self._train_every = {} # How many steps between updates of target network 
+        self._train_every = 64 # How many steps between updates of target network 
 
         self._sensor_ids = sensor_ids
         self._observation_time = observation_time
@@ -79,7 +79,6 @@ class WSN_agent:
             self._optimizer[reward_type] = [optim.AdamW(self._policy_net[reward_type][agent].parameters(), lr=LR, amsgrad=True) for agent in range(self._num_sensors)]
             self._loss[reward_type] = [[] for _ in range(self._num_sensors)]
 
-        memory_capacity = 256 
         self._memory = ReplayMemory(memory_capacity)
 
         self._steps_done = 0
@@ -199,6 +198,7 @@ class WSN_agent:
         return actions
     
     def train(self):
+        train_steps = 0
         for i_episode in range(self._num_episodes):
             # Initialize the environment and get its state
             print(self._env.reset())
@@ -218,8 +218,8 @@ class WSN_agent:
                 print(f'Similarity reward: {similarity_reward}')
                 
                 # Move the reward onto the correct device (memory, cpu, or gpu)
-                throughput_reward = torch.tensor(throughput_reward, device=device)
-                similarity_reward = torch.tensor(similarity_reward, device=device)
+                throughput_reward = torch.tensor(throughput_reward, device=device, dtype=torch.float32)
+                similarity_reward = torch.tensor(similarity_reward, device=device, dtype=torch.float32)
 
                 done = terminated or truncated
 
@@ -237,17 +237,22 @@ class WSN_agent:
                 # Perform one step of the optimization (on the policy network)
                 self._optimize_model()
 
-                for agent in range(self._num_sensors):
-                    # Update target networks
-                    for reward_type in self._reward_types:
-                        #if self._train_steps[reward_type] >= self._train_every[reward_type]:
-                        #self._train_steps[reward_type] = 0
-                        # Soft update of the target network's weights
-                        target_net_state_dict = self._target_net[reward_type][agent].state_dict()
-                        policy_net_state_dict = self._policy_net[reward_type][agent].state_dict()
-                        for key in policy_net_state_dict:
-                            target_net_state_dict[key] = policy_net_state_dict[key]*self._TAU + target_net_state_dict[key]*(1-self._TAU)
-                        self._target_net[reward_type][agent].load_state_dict(target_net_state_dict)
+                train_steps += 1                
+                with torch.no_grad():
+                    if train_steps >= self._train_every:
+                        self._train_steps = 0
+                        for agent in range(self._num_sensors):
+                            # Update target networks
+                            for reward_type in self._reward_types:
+                                self._target_net[reward_type][agent].load_state_dict(self._policy_net[reward_type][agent].state_dict())
+                            #if self._train_steps[reward_type] >= self._train_every[reward_type]:
+                            #self._train_steps[reward_type] = 0
+                            # Soft update of the target network's weights
+                            #target_net_state_dict = self._target_net[reward_type][agent].state_dict()
+                            #policy_net_state_dict = self._policy_net[reward_type][agent].state_dict()
+                            #for key in policy_net_state_dict:
+                            #    target_net_state_dict[key] = policy_net_state_dict[key]*self._TAU + target_net_state_dict[key]*(1-self._TAU)
+                            #self._target_net[reward_type][agent].load_state_dict(target_net_state_dict)
 
                 if done:
                     self._episode_durations.append(t + 1)
@@ -263,19 +268,20 @@ if __name__ == '__main__':
     sampling_freq = 4
     transmission_size = 2*1500
     observation_time = 1
-    local_mininet_simulation = True 
-    server_ip = "10.251.169.23" # IP of mininet simulation; ignored if local_mininet_simulation = True
+    local_mininet_simulation = False 
+    server_ip = "10.192.135.56" # IP of mininet simulation; ignored if local_mininet_simulation = True
     server_port = 5000 # Ignored if local_mininet_simulation = True
 
     # RL parametrs
-    BATCH_SIZE = 16 
-    GAMMA = 0.70
-    EPS_DECAY = 200 
-    EPS_START = 1
+    BATCH_SIZE = 64 
+    memory_capacity = 1024 
+    GAMMA = 0.99
+    EPS_DECAY = 3000 
+    EPS_START = 0.3
     EPS_END = 0
-    max_steps = 3000
+    max_steps = 9999 
     LR = 0.25e-2
     
-    agent = WSN_agent(sensor_ids=sensor_ids, sampling_freq=sampling_freq, transmission_size=transmission_size, observation_time=observation_time, BATCH_SIZE=BATCH_SIZE, max_steps=max_steps, LR=LR, EPS_DECAY=EPS_DECAY, EPS_START=EPS_START, EPS_END=EPS_END, GAMMA=GAMMA, local_mininet_simulation=local_mininet_simulation, server_ip=server_ip, server_port=server_port)
+    agent = WSN_agent(sensor_ids=sensor_ids, sampling_freq=sampling_freq, transmission_size=transmission_size, observation_time=observation_time, BATCH_SIZE=BATCH_SIZE, memory_capacity=memory_capacity, max_steps=max_steps, LR=LR, EPS_DECAY=EPS_DECAY, EPS_START=EPS_START, EPS_END=EPS_END, GAMMA=GAMMA, local_mininet_simulation=local_mininet_simulation, server_ip=server_ip, server_port=server_port)
     agent.train()
     
