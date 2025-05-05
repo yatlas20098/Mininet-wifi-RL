8a1bbb1bbd1ed8ccc01779efdf573232e0a9bb4d
yassir <yatlas20098@gmail.com>
2025-05-05 12:20:21 -0500

    Priortiy Replay Buffer

--- rl_agent.py
+++ rl_agent.py
@@ -17,7 +17,6 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torch.optim.lr_scheduler as lr_scheduler
 import torch.nn.init as init
-
 from WSN_env import WSNEnvironment
 from models import DDQN 
 
@@ -34,6 +33,167 @@ device = torch.device(
         )
 
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'throughput_reward', 'similarity_reward'))
+
+class SumTree:
+    def __init__(self, size):
+        self.nodes = [0] * (2 * size - 1)
+        self.data = [None] * size
+
+        self.size = size
+        self.count = 0
+        self.real_size = 0
+
+    @property
+    def total(self):
+        return self.nodes[0]
+
+    def update(self, data_idx, value):
+        idx = data_idx + self.size - 1  # child index in tree array
+        change = value - self.nodes[idx]
+
+        self.nodes[idx] = value
+
+        parent = (idx - 1) // 2
+        while parent >= 0:
+            self.nodes[parent] += change
+            parent = (parent - 1) // 2
+
+    def add(self, value, data):
+        self.data[self.count] = data
+        self.update(self.count, value)
+
+        self.count = (self.count + 1) % self.size
+        self.real_size = min(self.size, self.real_size + 1)
+
+    def get(self, cumsum):
+        #assert cumsum <= self.total
+
+        idx = 0
+        while 2 * idx + 1 < len(self.nodes):
+            left, right = 2*idx + 1, 2*idx + 2
+
+            if cumsum <= self.nodes[left]:
+                idx = left
+            else:
+                idx = right
+                cumsum = cumsum - self.nodes[left]
+
+        data_idx = idx - self.size + 1
+
+        return data_idx, self.nodes[idx], self.data[data_idx]
+
+    def __repr__(self):
+        return f"SumTree(nodes={self.nodes.__repr__()}, data={self.data.__repr__()})"
+
+class PrioritizedReplayMemory:
+    def __init__(self, buffer_size, eps=1e-2, alpha=0.1, beta=0.1):
+        self.tree = SumTree(size=buffer_size)
+
+        # PER params
+        self.eps = eps  # minimal priority, prevents zero probabilities
+        self.alpha = alpha  # determines how much prioritization is used, α = 0 corresponding to the uniform case
+        self.beta = beta  # determines the amount of importance-sampling correction, b = 1 fully compensate for the non-uniform probabilities
+        self.max_priority = eps  # priority for new samples, init as eps
+
+        # transition: state, action, reward, next_state, done
+        self.memory = [None for _ in range(buffer_size)] 
+        #self.state = torch.empty(buffer_size, state_size, dtype=torch.float)
+        #self.action = torch.empty(buffer_size, action_size, dtype=torch.float)
+        #self.reward = torch.empty(buffer_size, dtype=torch.float)
+        #self.next_state = torch.empty(buffer_size, state_size, dtype=torch.float)
+        #self.done = torch.empty(buffer_size, dtype=torch.int)
+
+        self.count = 0
+        self.real_size = 0
+        self.size = buffer_size
+    
+    def __len__(self):
+        return len(self.memory)
+
+    def push(self, *args):
+        #self.memory.append(Transition(*args))
+        self.memory[self.count] = Transition(*args)
+
+        #state, action, reward, next_state, done = transition
+
+        # store transition index with maximum priority in sum tree
+        self.tree.add(self.max_priority, self.count)
+
+        # store transition in the buffer
+        #self.state[self.count] = torch.as_tensor(state)
+        #self.action[self.count] = torch.as_tensor(action)
+        #self.reward[self.count] = torch.as_tensor(reward)
+        #self.next_state[self.count] = torch.as_tensor(next_state)
+        #self.done[self.count] = torch.as_tensor(done)
+
+        # update counters
+        self.count = (self.count + 1) % self.size
+        self.real_size = min(self.size, self.real_size + 1)
+
+    def sample(self, batch_size):
+        #assert self.real_size >= batch_size, "buffer contains less samples than batch size"
+
+        sample_idxs, tree_idxs = [], []
+        priorities = torch.empty(batch_size, 1, dtype=torch.float)
+
+        # To sample a minibatch of size k, the range [0, p_total] is divided equally into k ranges.
+        # Next, a value is uniformly sampled from each range. Finally the transitions that correspond
+        # to each of these sampled values are retrieved from the tree. (Appendix B.2.1, Proportional prioritization)
+        segment = self.tree.total / batch_size
+        for i in range(batch_size):
+            a, b = segment * i, segment * (i + 1)
+
+            cumsum = random.uniform(a, b)
+            # sample_idx is a sample index in buffer, needed further to sample actual transitions
+            # tree_idx is a index of a sample in the tree, needed further to update priorities
+            tree_idx, priority, sample_idx = self.tree.get(cumsum)
+
+            priorities[i] = priority
+            tree_idxs.append(tree_idx)
+            sample_idxs.append(sample_idx)
+
+        # Concretely, we define the probability of sampling transition i as P(i) = p_i^α / \sum_{k} p_k^α
+        # where p_i > 0 is the priority of transition i. (Section 3.3)
+        probs = priorities / self.tree.total
+
+        # The estimation of the expected value with stochastic updates relies on those updates corresponding
+        # to the same distribution as its expectation. Prioritized replay introduces bias because it changes this
+        # distribution in an uncontrolled fashion, and therefore changes the solution that the estimates will
+        # converge to (even if the policy and state distribution are fixed). We can correct this bias by using
+        # importance-sampling (IS) weights w_i = (1/N * 1/P(i))^β that fully compensates for the non-uniform
+        # probabilities P(i) if β = 1. These weights can be folded into the Q-learning update by using w_i * δ_i
+        # instead of δ_i (this is thus weighted IS, not ordinary IS, see e.g. Mahmood et al., 2014).
+        # For stability reasons, we always normalize weights by 1/maxi wi so that they only scale the
+        # update downwards (Section 3.4, first paragraph)
+        weights = (self.real_size * probs) ** -self.beta
+
+        # As mentioned in Section 3.4, whenever importance sampling is used, all weights w_i were scaled
+        # so that max_i w_i = 1. We found that this worked better in practice as it kept all weights
+        # within a reasonable range, avoiding the possibility of extremely large updates. (Appendix B.2.1, Proportional prioritization)
+        weights = weights / weights.max()
+
+        #batch = (
+        #    self.state[sample_idxs].to(device()),
+        #    self.action[sample_idxs].to(device()),
+        #    self.reward[sample_idxs].to(device()),
+        #    self.next_state[sample_idxs].to(device()),
+        #    self.done[sample_idxs].to(device())
+        #)
+        #return batch, weights, tree_idxs
+        return [self.memory[i] for i in sample_idxs], weights, tree_idxs
+
+    def update_priorities(self, data_idxs, priorities):
+        if isinstance(priorities, torch.Tensor):
+            priorities = priorities.detach().cpu().numpy()
+
+        for data_idx, priority in zip(data_idxs, priorities):
+            # The first variant we consider is the direct, proportional prioritization where p_i = |δ_i| + eps,
+            # where eps is a small positive constant that prevents the edge-case of transitions not being
+            # revisited once their error is zero. (Section 3.3)
+            priority = (priority + self.eps) ** self.alpha
+
+            self.tree.update(data_idx, priority)
+            self.max_priority = max(self.max_priority, priority)
 
 class ReplayMemory(object):
     def __init__(self, capacity):
@@ -79,7 +239,7 @@ class WSN_agent:
             self._optimizer[reward_type] = [optim.AdamW(self._policy_net[reward_type][agent].parameters(), lr=LR, amsgrad=True) for agent in range(self._num_sensors)]
             self._loss[reward_type] = [[] for _ in range(self._num_sensors)]
 
-        self._memory = ReplayMemory(memory_capacity)
+        self._memory = [PrioritizedReplayMemory(memory_capacity) for _ in range(self._num_sensors)]
 
         self._steps_done = 0
         self._episode_durations = []
@@ -96,13 +256,13 @@ class WSN_agent:
         self._state, self._info = self._env.reset()
 
     def _optimize_model(self):
-        if len(self._memory) < self._BATCH_SIZE:
+        if len(self._memory[0]) < self._BATCH_SIZE:
             return
         
         total_loss = {r:0 for r in self._reward_types} 
         for agent in range(self._num_sensors):
             # Sample a batch of transitions 
-            transitions = self._memory.sample(self._BATCH_SIZE)
+            transitions, weights, tree_idxs = self._memory[agent].sample(self._BATCH_SIZE)
             batch = Transition(*zip(*transitions))
 
             # Get the non-final states in the batch
@@ -130,6 +290,10 @@ class WSN_agent:
                     reward = similarity_reward[:, agent]
 
                 predicted_q_values = (next_state_values * self._GAMMA).squeeze() + reward
+                
+                if reward_type == 'throughput':
+                    td_error = (state_action_values - predicted_q_values).abs().detach().cpu()
+                    self._memory[agent].update_priorities(tree_idxs, td_error.numpy()) 
 
                 # Calculate Loss 
                 criterion = nn.SmoothL1Loss()
@@ -232,10 +396,9 @@ class WSN_agent:
                 else:
                     next_state = observation.clone().detach()
 
-                avg_reward = np.mean(throughput_reward[:-10])
-                if throughput_reward[0] < avg_reward: 
+                for i in range(self._num_sensors):
                     # Store the transition in memory
-                    self._memory.push(self._state, action, next_state, throughput_reward, similarity_reward)
+                    self._memory[i].push(self._state, action, next_state, throughput_reward, similarity_reward)
 
                 # Move to the next state
                 self._state = next_state
@@ -274,7 +437,7 @@ if __name__ == '__main__':
     sampling_freq = 4
     transmission_size = 2*1500
     observation_time = 1
-    local_mininet_simulation = False 
+    local_mininet_simulation = True 
     server_ip = "10.192.135.56" # IP of mininet simulation; ignored if local_mininet_simulation = True
     server_port = 5000 # Ignored if local_mininet_simulation = True
 
