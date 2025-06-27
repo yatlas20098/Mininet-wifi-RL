80f2eaa1e57c0c93ef2e0efd7f2fc51ba919bfd1
yassir <yatlas20098@gmail.com>
2025-06-27 14:40:57 -0500

    transfer

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 class Mininet_Simulation_Config:
-    def __init__(self, sensor_ids, sampling_freq=3, observation_time=10, transmission_size=4*1024, file_lines_per_chunk=5, transmission_frame_duration=1, local_simulation=True, remote_simulation_ip="", remote_simulation_port=""):
+    def __init__(self, sensor_ids, sampling_freq=3, observation_time=10, transmission_size=4*1024, file_lines_per_chunk=5, transmission_frame_duration=1, num_transmission_frames=10000, local_simulation=True, remote_simulation_ip="", remote_simulation_port=""):
         self.sensor_ids = sensor_ids
         self.sampling_freq = sampling_freq
         self.observation_time = observation_time
@@ -10,6 +10,7 @@ class Mininet_Simulation_Config:
         self.server_ip = remote_simulation_ip
         self.port = remote_simulation_port
         self.similarity_threshold = 1
+        self.num_transmission_frames = num_transmission_frames
 
 class Multi_Agent_PPO_Config:
     def __init__(self, batch_size=64, memory_capacity=256, gamma=0.99, tau=0.01, lr=0.0003, gae_lambda=0.95, policy_clip = 0.1, max_steps=100, num_episodes=10, train_every=2048, n_epochs=10):
